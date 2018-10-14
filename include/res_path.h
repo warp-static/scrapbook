546ae1e38da651c1158443ea5b3b3ca904a86a42
warp-static <rdorman34@gmail.com>
2018-10-14 21:41:36 +0100

    setup multi-target build and tested with a trash project

---
+++ include/res_path.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+
+std::string getResourcePath(const std::string &subDir = "")
+{
+    #ifdef _WIN32
+    const char PATH_SEP = '\\';
+    #else 
+    const char PATH_SEP = '/';
+    #endif
+
+    static std::string baseRes;
+    if(baseRes.empty())
+    {
+        char *basePath = SDL_GetBasePath();
+        if(basePath)
+        {
+            baseRes = basePath;
+            SDL_free(basePath);
+        }
+        else{
+            std::cerr << "Error getting resource path: " << SDL_GetError() << std::endl;
+            return "";
+        }
+        size_t pos = baseRes.rfind("bin");
+        baseRes = baseRes.substr(0, pos) + "res" + PATH_SEP;
+    }
+    return subDir.empty() ? baseRes : baseRes + subDir + PATH_SEP;
+}
